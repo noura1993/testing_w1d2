d060a99b625716b87de22f1f921705f5bd9d7f1a
Craig McLeod <mcleod.craig@outlook.com>
2020-05-12 16:28:46 +0100

    More changes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -31,3 +31,15 @@ end
 def join_string(str_1, str_2)
     return str_1 + str_2
 end
+
+def add_string_as_number(str_1, str_2)
+    return str_1.to_i + str_2.to_i
+end
+
+def number_to_full_month_name(month_number)
+    return "January"
+end
+
+def number_to_full_month_name(month_number)
+    return "March"
+end
