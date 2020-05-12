f94c4770ca72507c9ecbbc44d093c40ffb599a5b
Craig McLeod <mcleod.craig@outlook.com>
2020-05-12 15:57:21 +0100

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,13 @@
+# TODO: write implementation here
+
+def return_five()
+    return 2 + 3
+end
+
+def add(first_number, second_number)
+    return first_number + second_number
+end
+
+def return_10(first_number, second_number)
+    return first_number + second_number
+end
